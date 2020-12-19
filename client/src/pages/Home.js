faac30edf0e3bd2917d93507d8091a654feba6ee
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-12-19 11:36:04 -0800

    commpleted 21.6.4

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,8 @@ import ThoughtList from '../components/ThoughtList';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 
+import ThoughtForm from '../components/ThoughtForm';
+
 const Home = () => {
 	// use useQuery hook to make query request
 	const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -23,6 +25,11 @@ const Home = () => {
 	return (
 		<main>
 			<div className='flex-row justify-space-between'>
+				{loggedIn && (
+					<div className='col-12 mb-3'>
+						<ThoughtForm />
+					</div>
+				)}
 				<div
 					className={`col-12 mb-3 ${
 						loggedIn && 'col-lg-8'
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,11 +4,16 @@ import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/react-hooks';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
+import ThoughtForm from '../components/ThoughtForm';
+
 const Profile = (props) => {
+	const [addFriend] = useMutation(ADD_FRIEND);
+
 	const { username: userParam } = useParams();
 
 	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -35,6 +40,16 @@ const Profile = (props) => {
 		);
 	}
 
+	const handleClick = async () => {
+		try {
+			await addFriend({
+				variables: { id: user._id },
+			});
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	return (
 		<div>
 			<div className='flex-row mb-3'>
@@ -45,6 +60,14 @@ const Profile = (props) => {
 						: 'your'}{' '}
 					profile.
 				</h2>
+
+				{userParam && (
+					<button
+						className='btn ml-auto'
+						onClick={handleClick}>
+						Add Friend
+					</button>
+				)}
 			</div>
 
 			<div className='flex-row justify-space-between mb-3'>
@@ -62,6 +85,10 @@ const Profile = (props) => {
 						friends={user.friends}
 					/>
 				</div>
+			</div>
+
+			<div className='mb-3'>
+				{!userParam && <ThoughtForm />}
 			</div>
 		</div>
 	);
